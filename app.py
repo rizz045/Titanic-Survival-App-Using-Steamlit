1b4f447661f6e793a8ef06d2e8eec81e2801ca54
Rishabh Kumar Dhirhe <165001348+rizz045@users.noreply.github.com>
2025-01-17 23:33:49 +0530

    app.py

--- app.py
+++ app.py
@@ -4,48 +4,40 @@ import numpy as np
 import pickle
 import streamlit as st
 
-#load the model
+# Load the model
 with open("survival_model.pkl", "rb") as file:
     loaded_model = pickle.load(file)
 
 def survival(input_data):
-    inp_data_array = np.array(input_data)
-
-    inp_data_reshaped = inp_data_array.reshape(1, -1)
-
-    pred_in = loaded_model.predict(inp_data_reshaped)
-    # print(pred_in)
-
-    if pred_in[0] == 0:
-        return "The Person did not SURVIVE"
-    else:
-        return "The Person Survived"
-
+    try:
+        inp_data_array = np.array(input_data, dtype=float)
+        inp_data_reshaped = inp_data_array.reshape(1, -1)
+        pred_in = loaded_model.predict(inp_data_reshaped)
+        if pred_in[0] == 0:
+            return "The Person did not SURVIVE"
+        else:
+            return "The Person Survived"
+    except ValueError:
+        return "Invalid input. Please enter numeric values for all fields."
 
 def main():
-    # set title
     st.title("Titanic Survival Web Application")
-
-    # set header
     st.header("Enter the details of a passenger")
     
-    # input values from the user
-    Pclass = st.text_input("Enter Pclass (1, 2, 3)")
-    Sex = st.text_input("Enter Sex of a preson(1:male or 2:female)")
-    Age = st.text_input("Enter Age of the person(0-80)")
-    SibSp = st.text_input("Enter SibSp (0-5)")
-    Parch = st.text_input("Enter Parch (0-6)")
-    Fare = st.text_input("Enter Fare (0-512)")
-    Embarked = st.text_input("Enter Embarked from which place (0, 1, 2)")
+    # Input values from the user
+    Pclass = st.text_input("Enter Pclass (1, 2, 3)", value="1")
+    Sex = st.text_input("Enter Sex of a person (1:male or 2:female)", value="1")
+    Age = st.text_input("Enter Age of the person (0-80)", value="25")
+    SibSp = st.text_input("Enter SibSp (0-5)", value="0")
+    Parch = st.text_input("Enter Parch (0-6)", value="0")
+    Fare = st.text_input("Enter Fare (0-512)", value="50")
+    Embarked = st.text_input("Enter Embarked from which place (0, 1, 2)", value="0")
 
-    # code for prediction
-    answer = ""                  # empty string here the answer will be stored
+    answer = ""  # Placeholder for prediction result
 
-    #Creating a button for prediction
     if st.button("Predict Survival"):
         answer = survival([Pclass, Sex, Age, SibSp, Parch, Fare, Embarked])
-
-    st.success(answer)
+        st.success(answer)
 
 if __name__ == '__main__':
-    main() 
+    main()
